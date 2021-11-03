f7afd3e0691ea6b0a1f2e46d43d21515239d7143
CodingWithRick <nitubasukundu@gmail.com>
2021-11-03 09:07:52 +0530

    first commit

---
+++ configuration/apps.lua
@@ -0,0 +1,44 @@
+local filesystem = require('gears.filesystem')
+
+-- Thanks to jo148 on github for making rofi dpi aware!
+local with_dpi = require('beautiful').xresources.apply_dpi
+local get_dpi = require('beautiful').xresources.get_dpi
+local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. with_dpi(400) .. ' -show drun -theme ' .. filesystem.get_configuration_dir() .. '/configuration/rofi.rasi -run-command "/bin/bash -c -i \'shopt -s expand_aliases; {cmd}\'"'
+
+return {
+  -- List of apps to start by default on some actions
+  default = {
+    terminal = 'alacritty',
+    rofi = "dmenu_run -p Run: -i -nb '#192832' -nf '#fefffe' -sb '#fefffe' -sf '#191919' -fn 'FiraMono:pixelsize=22'",
+    lock = 'i3lock-fancy',
+    quake = 'alacritty',
+    screenshot = 'flameshot screen -p ~/Pictures/Screenshots',
+    region_screenshot = 'flameshot gui -p ~/Pictures/Screenshots',
+    delayed_screenshot = 'flameshot screen -p ~/Pictures/Screenshots -d 5000',
+    browser = 'brave',
+    editor = 'code', -- gui text editor
+    social = 'discord',
+    game = 'visualboyadvance-m',
+    files = 'pcmanfm',
+    music = 'vlc'
+  },
+  -- List of apps to start once on start-up
+  run_on_start_up = {
+    -- 'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
+    -- 'nm-applet --indicator', -- wifi
+    -- 'pnmixer', -- shows an audiocontrol applet in systray when installed.
+    -- --'blueberry-tray', -- Bluetooth tray icon
+    -- 'numlockx on', -- enable numlock
+    -- '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    -- 'xfce4-power-manager', -- Power manager
+    -- 'flameshot',
+    -- 'synology-drive -minimized',
+    -- 'nitrogen --restore',
+    -- --  'steam -silent',
+    -- -- 'feh --randomize --bg-fill ~/.wallpapers/*',
+    -- -- '/usr/bin/variety',
+    -- -- Add applications that need to be killed between reloads
+    -- -- to avoid multipled instances, inside the awspawn script
+    -- '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+  }
+}
